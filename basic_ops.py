c139a913d97f1f44cfef8d1e1cf4d81124dc31fb
Xiaoyang <shix@mit.edu>
2021-01-30 17:55:47 -0500

    Update basic_ops.py

--- basic_ops.py
+++ basic_ops.py
@@ -17,28 +17,25 @@ provider = IonQProvider(token=key)#Call provider and set token value
 
 # initialize board with side length n (currently allows 1 (trivial), 2 and 3). Adds an ancilla qubit for each team. Each qubit is associated with a classical channel for readout.
 def initialize(n):
-    qc = QuantumCircuit(n*n,n*n)
-    qc.h(range(n*n))
-    return qc
+    circ = QuantumCircuit(n*n,n*n)
+    circ.h(range(n*n))
+    return circ
 
 # entangle the team qubit with a chosen location on the board (numbered starting at 1 from the upper left corner and proceeding left to right by row.)
-def entangle(qcircuit,control,target):
-    qc=qcircuit
-    qc.cx(control,target)
-    return qc
+def add_cnot(circ,control,target):
+    circ.cx(control,target)
+    return circ
 
 #rotation
-def rotate(qcircuit, team, loc):
-	qc=qcircuit
+def add_rz(circ, team, loc):
 	if team==1:
 		theta = math.pi/2.
 	else: theta = -math.pi/2.
-	qc.rz(theta, loc)
-	return qc
+	circ.rz(theta, loc)
+	return circ
 
 
 #measure final state, n is the side length of the board
-def measure(qcircuit,n):
-	qc=qcircuit
-	qc.measure(range(n*n),range(n*n))
-	return qc
+def add_measurement(circ,n):
+	circ.measure(range(n*n),range(n*n))
+	return circ
