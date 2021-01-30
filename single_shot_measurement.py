18d072c9bf1a612b7a161e18f6e1295b47b16bcc
Xiaoyang <shix@mit.edu>
2021-01-30 18:08:17 -0500

    updates

--- single_shot_measurement.py
+++ single_shot_measurement.py
@@ -1,1 +1,12 @@
-s
+from qiskit import Aer, execute
+from qiskit_ionq_provider import IonQProvider 
+from qiskit.visualization import plot_state_city
+from qiskit.visualization import plot_histogram
+from qiskit.providers.jobstatus import JobStatus
+from qiskit import QuantumCircuit
+import math
+
+with open(".ionqkey.txt") as keyfile:
+    key = keyfile.readline().strip('\n')
+#Call provider and set token value
+provider = IonQProvider(token=key)#Call provider and set token value
