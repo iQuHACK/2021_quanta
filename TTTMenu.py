2febb726aeb6bf7d5558662643bd54f993a6db76
Kyle DeBry <debry.1@osu.edu>
2021-01-30 18:07:59 -0500

    initial gui commit

---
+++ TTTMenu.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 16:34:14 2021
+
+@author: ked
+"""
+
+import pygame as pg
+
+HIGHLIGHT = (255, 200, 200)
+MOUSEOVER = (160, 160, 255)
+
+class TTTMenu:
+    
+    def __init__(self, screen, item_size, pos, pad, color):
+        self.screen = screen
+        self.item_size = item_size
+        self.loc_x = pos[0]
+        self.loc_y = pos[1]
+        self.pad = pad
+        self.color = color
+        self.w = self.item_size + 2 * self.pad
+        self.h = 4 * (self.item_size + 4 * self.pad)
+        
+        self.items = ["rot_minus", "rot_plus", "cnot", "measure"]
+        
+        self.highlighted = []
+        self.mouseover_item = None
+    
+    def draw(self):
+        
+        sq_size = int(self.item_size + 2 * self.pad)
+        
+        for row in range(4):
+            x = int(self.loc_x)
+            y = int(self.loc_y + row * (self.item_size + 4 * self.pad))
+            if self.items[row] in self.highlighted:
+                color = HIGHLIGHT
+            elif self.mouseover_item == self.items[row]:
+                color = MOUSEOVER
+            else:
+                color = self.color
+            pg.draw.rect(self.screen, color, (x, y, sq_size, sq_size))
+            image = pg.image.load(self.items[row] + ".png").convert_alpha()
+            self.screen.blit(image, (x + self.pad, y + self.pad))
+        self.mouseover_item = None
+            
+        
+    def find_item(self, pos):
+        if self.loc_x < pos[0] < self.loc_x + self.w:
+            if self.loc_y < pos[1] < self.loc_y + self.h:
+                row = int((pos[1] - self.loc_y) // (self.item_size + 4 * self.pad))
+                row = min(row, len(self.items))
+            else:
+                return None
+        else:
+            return None
+        
+        return self.items[row]
+    
+    def highlight(self, item):
+        if item not in self.highlighted:
+            self.highlighted.append(item)
+    
+    def mouseover(self, item):
+        if item is not None:
+            self.mouseover_item = item
+    
+    def reset(self):
+        self.highlighted = []
---
+++ TiqTaqToe.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 13:06:52 2021
+
+@author: ked
+"""
+
+import sys
+from enum import Enum, auto
+import pygame as pg
+from TTTBoard import TTTBoard
+from TTTMenu import TTTMenu
+
+class TiqTaqToe:
+    
+    class State(Enum):
+        CHOOSE_GATE = auto()
+        CHOOSE_2Q_CONTROL = auto()
+        CHOOSE_2Q_TARGET = auto()
+        CHOOSE_1Q_TARGET = auto()
+    
+    def __init__(self, tiles=3, w=1000, h=800):
+        self.tiles = tiles
+        self.w = w
+        self.h = h        
+        
+        pg.init()
+        pg.display.set_caption('Tiq-Taq-Toe')
+        self.screen = pg.display.set_mode((self.w, self.h))
+        self.clock = pg.time.Clock()
+        
+        self.board = TTTBoard(self.screen, self.tiles, w/2, (w/4, h/4), 10, (200,200,200), 0)
+        self.board.draw_board()
+        
+        self.menu = TTTMenu(self.screen, 100, (20, 20), 10, (200,200,255))
+        
+        self.font = pg.font.SysFont('Corbel', 80)
+        self.font_small = pg.font.SysFont('Corbel', 40)
+        self.p1_text = self.font.render('Player 1', True, (0,0,0))
+        self.p2_text = self.font.render('Player 2', True, (0,0,0))
+        self.gate_text = self.font_small.render('choose a gate', True, (0,0,0))
+        self.target_text = self.font_small.render('choose a target qubit', True, (0,0,0))
+        self.control_text = self.font_small.render('choose a control qubit', True, (0,0,0))
+        
+    def run(self):
+        playing = True
+        state = self.State.CHOOSE_GATE
+        control_qubit = None
+        gate = None
+        player1 = True
+        
+        while True:
+            self.screen.fill((255, 255, 255))
+            self.board.draw_board()
+            self.menu.draw()
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    qubit = self.board.find_qubit(event.pos)
+                    if qubit is not None:
+                        if state == self.State.CHOOSE_2Q_CONTROL:
+                            control_qubit = qubit
+                            self.board.highlight(qubit.row, qubit.col)
+                            state = self.State.CHOOSE_2Q_TARGET
+                            
+                        elif state is self.State.CHOOSE_2Q_TARGET:
+                            # TODO: Act on both qubits with gate
+                            
+                            self.board.reset()
+                            self.menu.reset()
+                            state = self.State.CHOOSE_GATE
+                            player1 = not player1
+                            
+                        elif state is not self.State.CHOOSE_GATE:
+                            # TODO: Act on qubit with gate
+                            state = self.State.CHOOSE_GATE
+                            self.menu.reset()
+                            player1 = not player1
+                        
+                    menu_item = self.menu.find_item(event.pos)
+                    if menu_item is not None:
+                        if menu_item == 'measure':
+                            # TODO: Measure
+                            pass
+                        else:
+                            if state is not self.State.CHOOSE_2Q_TARGET:
+                                self.menu.reset()
+                                gate = menu_item
+                                self.menu.highlight(menu_item)
+                                if gate == 'cnot':
+                                    state = self.State.CHOOSE_2Q_CONTROL
+                                else:
+                                    state = self.State.CHOOSE_1Q_TARGET
+                            
+            mouse_pos = pg.mouse.get_pos()
+            mouse_qubit = self.board.find_qubit(mouse_pos)
+            mouse_menu_item = self.menu.find_item(mouse_pos)
+
+            if state is not self.State.CHOOSE_2Q_TARGET:
+                self.menu.mouseover(mouse_menu_item)
+            if state is not self.State.CHOOSE_GATE:
+                self.board.mouseover(mouse_qubit)
+            
+            
+            if player1:
+                self.screen.blit(self.p1_text, (400, 20))
+            else:
+                self.screen.blit(self.p2_text, (400, 20))
+            
+            if state is self.State.CHOOSE_GATE:
+                self.screen.blit(self.gate_text, (400, 80))
+            elif state is self.State.CHOOSE_2Q_CONTROL:
+                self.screen.blit(self.control_text, (400, 80))
+            else:
+                self.screen.blit(self.target_text, (400, 80))
+            
+            pg.display.update()
+            self.clock.tick(30)
+
+game = TiqTaqToe()
+game.run()
